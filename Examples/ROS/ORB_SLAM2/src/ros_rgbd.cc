385225abd34faae21c4342ad0fab6fbe8a609d0a
Hyundo Lee <illhyhl1111@snu.ac.kr>
2018-02-19 18:12:22 +0900

    add /orb_base_link transformation

--- Examples/ROS/ORB_SLAM2/src/ros_rgbd.cc
+++ Examples/ROS/ORB_SLAM2/src/ros_rgbd.cc
@@ -194,55 +194,22 @@ void ImageGrabber::GrabRGBD(const sensor_msgs::ImageConstPtr& msgRGB,const senso
 	tf::Transform transformMap = tf::Transform(map_quaternion, map_translation);
 	br.sendTransform(tf::StampedTransform(transformMap, ros::Time::now(), "map", "orb_map"));
 
-	//rotate 270deg about z and 270deg about x
-	/*tf::Matrix3x3 rotation270degZX(0, 0, 1,
-									-1, 0, 0,
-									0, -1, 0);*/
-
-	//publish right handed, x forward, y right, z down (NED)
-	//tf::Transform transformCoordSystem = tf::Transform(rotation270degZX, tf::Vector3(0.0, 0.0, 0.0));
-	//br.sendTransform(tf::StampedTransform(transformCoordSystem, ros::Time::now(), "asdf", "orb_map"));
+	tf::TransformListener listener_link;
+	tf::StampedTransform transform_link;
+	try {
+		listener_link.lookupTransform("/CameraTop_optical_frame", "/base_link", ros::Time(0), transform_link);
+		transform_link.frame_id_ = "/orb_pose";
+		transform_link.child_frame_id_ = "/orb_base_link";
+		br.sendTransform(transform_link);
+	}
+	catch (tf::TransformException ex) {
+		ROS_ERROR("%s", ex.what());
+		ros::Duration(1.0).sleep();
+	}
 
 	tf::Transform transformCamera = tf::Transform(rh_cameraPose, rh_cameraTranslation);
 	br.sendTransform(tf::StampedTransform(transformCamera.inverse(), ros::Time::now(), "orb_map", "orb_pose"));
 
-/*
-	
-    // global left handed coordinate system
-    static cv::Mat pose_prev = cv::Mat::eye(4,4, CV_32F);
-    static cv::Mat world_lh = cv::Mat::eye(4,4, CV_32F);
-    // matrix to flip signs of sinus in rotation matrix, not sure why we need to do that
-    static const cv::Mat flipSign = (cv::Mat_<float>(4,4) <<   1,-1,-1, 1,
-                                                               -1, 1,-1, 1,
-                                                               -1,-1, 1, 1,
-                                                                1, 1, 1, 1);
-
-    //prev_pose * T = pose
-    cv::Mat translation =  (pose * pose_prev.inv()).mul(flipSign);
-    world_lh = world_lh * translation;
-    pose_prev = pose.clone();
-
-
-    // transform into global right handed coordinate system, publish in ROS
-    tf::Matrix3x3 cameraRotation_rh(  - world_lh.at<float>(0,0),   world_lh.at<float>(0,1),   world_lh.at<float>(0,2),
-                                  - world_lh.at<float>(1,0),   world_lh.at<float>(1,1),   world_lh.at<float>(1,2),
-                                    world_lh.at<float>(2,0), - world_lh.at<float>(2,1), - world_lh.at<float>(2,2));
-
-    tf::Vector3 cameraTranslation_rh( world_lh.at<float>(0,3),world_lh.at<float>(1,3), - world_lh.at<float>(2,3) );
-
-    //rotate 270deg about x and 270deg about x to get ENU: x forward, y left, z up
-    const tf::Matrix3x3 rotation270degXZ(   0, 1, 0,
-                                            0, 0, 1,
-                                            1, 0, 0);
-
-    //static tf::TransformBroadcaster br;
-
-    tf::Matrix3x3 globalRotation_rh = cameraRotation_rh * rotation270degXZ;
-    tf::Vector3 globalTranslation_rh = cameraTranslation_rh * rotation270degXZ;
-    tf::Transform transform = tf::Transform(globalRotation_rh, globalTranslation_rh);
-    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "orb_map_", "orb_pose_"));
-*/
-	
 	//publish map
 	/*tf::TransformListener listener_link;
 	tf::TransformListener listener_pose;
