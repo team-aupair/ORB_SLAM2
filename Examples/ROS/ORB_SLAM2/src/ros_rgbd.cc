9a700992c7a4ee969a050a34728fdadf6eedbeba
Hyundo Lee <illhyhl1111@snu.ac.kr>
2018-03-09 19:10:21 +0900

    map drawer

--- Examples/ROS/ORB_SLAM2/src/ros_rgbd.cc
+++ Examples/ROS/ORB_SLAM2/src/ros_rgbd.cc
@@ -34,6 +34,8 @@
 
 #include "geometry_msgs/TransformStamped.h"
 #include "../../../include/System.h"
+#include "../../../include/Map.h"
+#include "../../../include/MapPoint.h"
 #include "tf/transform_datatypes.h"
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
@@ -41,8 +43,18 @@
 #include <math.h>
 #include <vector>
 #include <signal.h>
+#include <visualization_msgs/Marker.h>
 
 volatile sig_atomic_t flag = 0;
+double orb_scale = 1.0;
+vector<double> orb_trans;
+vector<double> orb_quat;
+tf::Transform map_transform;
+
+ORB_SLAM2::Map* mpMap;
+
+const int map_update_rate = 5;
+int update_counter = map_update_rate;
 
 using namespace std;
 
@@ -51,17 +63,62 @@ class ImageGrabber
 public:
     ImageGrabber(ORB_SLAM2::System* pSLAM):mpSLAM(pSLAM){}
 
-    void GrabRGBD(const sensor_msgs::ImageConstPtr& msgRGB,const sensor_msgs::ImageConstPtr& msgD, ros::NodeHandle& nh);
+    void GrabRGBD(const sensor_msgs::ImageConstPtr& msgRGB,const sensor_msgs::ImageConstPtr& msgD, ros::NodeHandle& nh, ros::Publisher marker_pub);
 
     ORB_SLAM2::System* mpSLAM;
 };
 
-double orb_scale = 1.0;
-vector<double> orb_trans;
-vector<double> orb_quat;
-
 void keyboard_inturrupt(int sig){
     flag = 1;
+}
+
+void publish_points(visualization_msgs::Marker& points, float x, float y, float z)
+{
+	tf::Vector3 pose(x*orb_scale, z*orb_scale, y*orb_scale);
+	tf::Vector3 transformed_pose = map_transform * pose;
+
+	geometry_msgs::Point p;
+	p.x = (float) transformed_pose.getX();
+	p.y = (float) transformed_pose.getY();
+	p.z = (float) transformed_pose.getZ();
+	points.points.push_back(p);
+}
+
+void get_map_points(visualization_msgs::Marker& points)
+{
+	points.header.frame_id = "/map";
+	points.action = visualization_msgs::Marker::ADD;
+	points.pose.orientation.w = 1.0;
+	points.id = 0;
+	points.type = visualization_msgs::Marker::POINTS;
+	points.scale.x = 0.025;
+	points.scale.y = 0.025;
+	points.color.b = 1.0f;
+	points.color.a = 1.0;
+
+	tf::Vector3 map_translation(orb_trans[0], orb_trans[1], orb_trans[2]);
+	tf::Quaternion map_quaternion(orb_quat[0], orb_quat[1], orb_quat[2], orb_quat[3]);
+	map_transform = tf::Transform(map_quaternion, map_translation);
+
+	const vector<ORB_SLAM2::MapPoint*> &vpMPs = mpMap->GetAllMapPoints();
+	const vector<ORB_SLAM2::MapPoint*> &vpRefMPs = mpMap->GetReferenceMapPoints();
+
+
+	set<ORB_SLAM2::MapPoint*> spRefMPs(vpRefMPs.begin(), vpRefMPs.end());
+
+	if(vpMPs.empty()){
+		return;
+	}
+
+	for(size_t i=0, iend=vpMPs.size(); i<iend;i++)
+	{
+		//if(vpMPs[i]->isBad() || spRefMPs.count(vpMPs[i]))
+		if(vpMPs[i]->isBad())
+			continue;
+		cv::Mat pos = vpMPs[i]->GetWorldPos();
+		publish_points(points, pos.at<float>(0),pos.at<float>(1),pos.at<float>(2));
+	
+	}
 }
 
 int main(int argc, char **argv)
@@ -77,7 +134,8 @@ int main(int argc, char **argv)
     }    
 
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
-    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::RGBD,true, true);
+	ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::RGBD,true, true);
+	mpMap = SLAM.getMap();
 
 	ros::NodeHandle nh;
 
@@ -96,6 +154,8 @@ int main(int argc, char **argv)
 
 	nh.setParam("orb_translation", orb_trans);
 	nh.setParam("orb_quaternion", orb_quat);
+
+	ros::Publisher marker_pub = nh.advertise<visualization_msgs::Marker> ("map_marker", 10);
  
     ImageGrabber igb(&SLAM);
 
@@ -103,7 +163,7 @@ int main(int argc, char **argv)
     message_filters::Subscriber<sensor_msgs::Image> depth_sub(nh, "/pepper_robot/camera/depth/image_raw", 1);
     typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> sync_pol;
     message_filters::Synchronizer<sync_pol> sync(sync_pol(10), rgb_sub,depth_sub);
-    sync.registerCallback(boost::bind(&ImageGrabber::GrabRGBD,&igb,_1,_2,nh));
+    sync.registerCallback(boost::bind(&ImageGrabber::GrabRGBD,&igb,_1,_2,nh,marker_pub));
 
     signal(SIGINT, keyboard_inturrupt);
 
@@ -126,7 +186,7 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void ImageGrabber::GrabRGBD(const sensor_msgs::ImageConstPtr& msgRGB,const sensor_msgs::ImageConstPtr& msgD, ros::NodeHandle& nh)
+void ImageGrabber::GrabRGBD(const sensor_msgs::ImageConstPtr& msgRGB,const sensor_msgs::ImageConstPtr& msgD, ros::NodeHandle& nh, ros::Publisher marker_pub)
 {
     // Copy the ros image message to cv::Mat.
     cv_bridge::CvImageConstPtr cv_ptrRGB;
@@ -250,6 +310,14 @@ void ImageGrabber::GrabRGBD(const sensor_msgs::ImageConstPtr& msgRGB,const senso
 
 	cout << "CAMERA_ODOM : (" << transform_link.getOrigin().x() << ", " << transform_link.getOrigin().y() << ", " << transform_link.getOrigin().z() << ")" << endl;
 	cout << "CAMERA_POSE : (" << transform_pose.getOrigin().x() << ", " << transform_pose.getOrigin().y() << ", " << transform_pose.getOrigin().z() << ")" << endl;*/
+
+	if(update_counter++ == map_update_rate) {
+		visualization_msgs::Marker points;
+		get_map_points(points);
+		points.header.stamp = ros::Time::now();
+		marker_pub.publish(points);
+		update_counter = 1;
+	}
 }
 
 
